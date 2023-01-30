42c910a0a3b13357cb00ad417f3202d404df2c76
Bolibek <fayzullayevbolibek@gmail.com>
2023-01-30 21:57:31 +0900

    Script.js refactored

--- alphabet/script.js
+++ alphabet/script.js
@@ -1,14 +1,20 @@
-document.addEventListener("keydown", (e) => {
+const playLetterSounds = (e) => {
 	const audio = document.querySelector(`audio[data-key="${e.code}"]`);
 	const key = document.querySelector(`div[data-key="${e.code}"]`);
 	if (!audio) return;
 	key.classList.add("playing");
 	audio.currentTime = 0;
 	audio.play();
-});
-const keys = Array.from(document.querySelectorAll(".key"));
-keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+};
+
 function removeTransition(e) {
 	if (e.propertyName !== "transform") return;
 	e.target.classList.remove("playing");
 }
+
+document.addEventListener("keydown", playLetterSounds);
+
+const keys = Array.from(document.querySelectorAll(".key"));
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+
+
